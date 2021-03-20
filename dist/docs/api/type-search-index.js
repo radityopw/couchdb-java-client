722ea3c1cc2f104e14c6ef90438e30986ca1a8d1
radityopw <radityo_pw@is.its.ac.id>
2021-03-20 20:39:45 +0700

    initial commit

--- dist/docs/api/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"radityopw.couchdbclient","l":"CouchdbClient"},{"p":"radityopw.couchdbclient","l":"CouchdbClientConfig"}];updateSearchResults();
