295c8c241e90adbb337bad0e6ceff7ced5443c57
radityopw <radityo_pw@is.its.ac.id>
2021-04-05 20:16:16 +0700

    custom unit testing

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,15 +6,148 @@ import java.util.logging.*;
 import org.json.*;
 
 public class Test{
+	
+	public static CouchdbClientConfig getConfig(){
+		CouchdbClientConfig config = new CouchdbClientConfig();
+		config.setDatabase("shared");
+		
+		return config;
+	}
+	
+	public static void testUpsertData() throws Exception{
+		
+		boolean isError = false;
+		String errorMessage = null;
+		
+		CouchdbClient client = new CouchdbClient(getConfig());
+		JSONObject doc1 = new JSONObject("{\"_id\":\"user:coba\",\"username\":\"admin\",\"password\":\"0192023a7bbd73250516f069df18b500\",\"role\":\"admin\"}");
+		JSONObject result1 = client.setDoc("user:coba",doc1);
+		
+		if(!result1.getString("id").equals("user:coba")){
+			isError = true;
+			errorMessage = "Test Upsert Data : id tidak sama";
+		}
+		
+		if(!result1.getBoolean("ok")){
+			isError = true;
+			errorMessage = "Test Upsert Data : result tidak OK";
+		}
+		
+		try{
+			// error atau tidak, coba hapus saja data yang diinput tadi
+			client.delDoc("user:coba");
+			
+		}catch(Exception e){}
+		
+		if(isError){
+			throw new Exception(errorMessage);
+		}
+	}
+	
+	public static void testListSemuaData() throws Exception{
+		boolean isError = false;
+		String errorMessage = null;
+		
+		CouchdbClient client = new CouchdbClient(getConfig());
+		
+		JSONObject data = client.allDocs();
+		
+		if(data.getInt("total_rows") != 1){
+			isError = true;
+			errorMessage = "Test List Semua Data : jumlah baris tidak 1";
+		}
+		
+		if(isError){
+			throw new Exception(errorMessage);
+		}
+	}
+	
+	public static void testListPartition() throws Exception{
+		boolean isError = false;
+		String errorMessage = null;
+		
+		CouchdbClient client = new CouchdbClient(getConfig());
+		
+		JSONObject doc1 = new JSONObject("{\"_id\":\"user:coba\",\"username\":\"admin\",\"password\":\"0192023a7bbd73250516f069df18b500\",\"role\":\"admin\"}");
+		JSONObject result1 = client.setDoc("user:coba",doc1);
+		
+		if(!result1.getString("id").equals("user:coba")){
+			isError = true;
+			errorMessage = "Test List Partition : id tidak sama";
+		}
+		
+		if(!result1.getBoolean("ok")){
+			isError = true;
+			errorMessage = "Test List Partition : result tidak OK";
+		}
+		
+		JSONObject data = client.partition("user");
+		
+		//System.out.println(data);
+		
+		if(data.getInt("total_rows") != 2){
+			isError = true;
+			errorMessage = "Test List Partition : jumlah baris tidak 2";
+		}
+		
+		try{
+			// error atau tidak, coba hapus saja data yang diinput tadi
+			client.delDoc("user:coba");
+			
+		}catch(Exception e){}
+		
+		if(isError){
+			throw new Exception(errorMessage);
+		}
+	}
+	
+	public static void testDeleteData() throws Exception{
+		
+		boolean isError = false;
+		String errorMessage = null;
+		
+		CouchdbClient client = new CouchdbClient(getConfig());
+		JSONObject doc1 = new JSONObject("{\"_id\":\"user:coba\",\"username\":\"admin\",\"password\":\"0192023a7bbd73250516f069df18b500\",\"role\":\"admin\"}");
+		
+		client.setDoc("user:coba",doc1);
+		
+		JSONObject result1 = client.delDoc("user:coba");
+			
+		if(!result1.getString("id").equals("user:coba")){
+			isError = true;
+			errorMessage = "Test Delete Data : id tidak sama";
+		}
+		
+		if(!result1.getBoolean("ok")){
+			isError = true;
+			errorMessage = "Test Delete Data : result tidak OK";
+		}
+		
+		if(isError){
+			throw new Exception(errorMessage);
+		}
+	}
+	
 	public static void main(String[] a){
 		
 		try{
 			
-			Logger log = Logger.getLogger( CouchdbClient.class.getName() );
-			ConsoleHandler handler = new ConsoleHandler();
-			handler.setFormatter(new SimpleFormatter());
-			handler.setLevel(Level.ALL);
-			log.addHandler(handler);
+			testUpsertData();
+			testListSemuaData();
+			testListPartition();
+			testDeleteData();
+			
+			System.out.println("OK tidak ada error");
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}	
+		
+		/*
+		
+		try{
+			
+			
 			
 			CouchdbClientConfig config = new CouchdbClientConfig();
 			config.setDatabase("shared");
@@ -52,5 +185,7 @@ public class Test{
 			System.out.println("Error Terjadi!!");
 			System.out.println(e);
 		}
+		
+		*/
 	}
 }
